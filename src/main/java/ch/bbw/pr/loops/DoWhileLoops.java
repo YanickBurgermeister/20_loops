2362eb35673bd989117cb43cc47272ec82f79fb4
Peter Rutschmann <peter.rutschmann@bbw.ch>
2021-11-07 13:28:41 +0100

    Erste Version bereitstellen.

---
+++ src/main/java/ch/bbw/pr/loops/DoWhileLoops.java
@@ -0,0 +1,81 @@
+package ch.bbw.pr.loops;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Do-WhileLoops
+ *   Methoden gemäss Anforderung implementieren.
+ *   Unittest ausführen.
+ * @author Peter Rutschmann
+ * @author <hier Ihren Namen eintragen>
+ * @version 05.11.2021
+ */
+public class DoWhileLoops {
+
+   /** 1. Alle Zahlen von 0 bis 20 ausgeben. */
+   public List<Integer> doWhile0to20(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      //do ....
+          //list.add ...
+      //while ...
+      return list;
+   }
+
+   /** 2. Alle Zahlen von 25 bis 50 ausgeben. */
+   public List<Integer> doWhile25to50(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 3. Alle Zahlen von -25 bis 25 ausgeben. */
+   public List<Integer> doWhileM25to25(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 4. Alle Zahlen der 4-Reihe von 4 bis 40 ausgeben. */
+   public List<Integer> doWhile4er4to40(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 5. Alle Zahlen der 9-Reihe von 9 bis 81 ausgeben. */
+   public List<Integer> doWhile9er9to81(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 6. Alle Zahlen der 11-Reihe von 44 bis 121 ausgeben. */
+   public List<Integer> doWhile11er44to121(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 7. Alle Zahlen der 8-Reihe von 96 bis 16 ausgeben. */
+   public List<Integer> doWhile8er96to16(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 8. Alle Zahlen der 25-Reihe von 400 bis 100 ausgeben. */
+   public List<Integer> doWhile25er400to100(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 9. Alle Zahlen der 13-Reihe von 2197 bis 2028 ausgeben. */
+   public List<Integer> doWhile13er2197to2028(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+}
---
+++ src/main/java/ch/bbw/pr/loops/ForLoops.java
@@ -0,0 +1,94 @@
+package ch.bbw.pr.loops;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * For-Loops
+ *   Methoden gemäss Anforderung implementieren.
+ *   Unittest ausführen.
+ * @author Peter Rutschmann
+ * @author <hier Ihren Namen eintragen>
+ * @version 05.11.2021
+ */
+public class ForLoops {
+
+   /** 1. Alle Zahlen von 1 bis 50 ausgeben. */
+   public List<Integer> for1to50(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      //for ....
+          //list.add ...
+      return list;
+   }
+
+   /** 2. Alle Zahlen von 0 bis 30 ausgeben. */
+   public List<Integer> for0to30(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 3. Alle Zahlen von 50 bis 100 ausgeben. */
+   public List<Integer> for50to100(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 4. Alle Zahlen von 40 bis 0 ausgeben. */
+   public List<Integer> for40to0(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 5. Alle Zahlen von 200 bis 180 ausgeben. */
+   public List<Integer> for200to180(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 6. Alle Zahlen der 2-Reihe von 2 bis 20 ausgeben. */
+   public List<Integer> for2er2to20(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 7. Alle Zahlen der 7-Reihe von 7 bis 70 ausgeben. */
+   public List<Integer> for7er7to70(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 8. Alle Zahlen der 12-Reihe von 60 bis 120 ausgeben. */
+   public List<Integer> for12er60to120(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 9. Alle Zahlen der 5-Reihe von 125 bis 0 ausgeben. */
+   public List<Integer> for5er125to0(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 10. Alle Zahlen der 25-Reihe von 600 bis 200 ausgeben. */
+   public List<Integer> for25er600to200(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+
+   /** 11. Alle Zahlen der 13-Reihe von 2197 bis 2028 ausgeben. */
+   public List<Integer> for13er2197to2028(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      return list;
+   }
+}
