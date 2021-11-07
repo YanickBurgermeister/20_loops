016a0ed48a798b319c42037a1edf9c6df07ea8f8
Peter Rutschmann <peter.rutschmann@bbw.ch>
2021-11-07 14:47:02 +0100

    Übungen zu Quadrat und Kubik hinzugefügt.

---
+++ src/main/java/ch/bbw/pr/loops/SequenceOfNumbers.java
@@ -0,0 +1,64 @@
+package ch.bbw.pr.loops;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * SequenceOfNumbers
+ *   Weitere Zahlenreihen
+ *   Methoden gemäss Anforderung implementieren.
+ *   Unittest ausführen.
+ * @author Peter Rutschmann
+ * @author <hier Ihren Namen eintragen>
+ * @version 05.11.2021
+ */
+public class SequenceOfNumbers {
+
+   /** 1. For-Loop: Alle Quadratzahlen für die Zahlen 1 bis 5*/
+   public List<Integer> forQuadrat1to5(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      //for ....
+          //list.add ...
+      return list;
+   }
+
+   /** 2. While-Loop: Alle Quadratzahlen für die Zahlen 5 bis 10*/
+   public List<Integer> whileQuadrat5to10(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      //while ....
+          //list.add ...
+      return list;
+   }
+
+   /** 3. Do While-Loop: Alle Quadratzahlen für die Zahlen 10 bis 15*/
+   public List<Integer> doWhileQuadrat10to15(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+      //do
+      //list.add ...
+          //while ....
+      return list;
+   }
+
+   /** 4. Alle Quadratzahlen zwischen 1 und 1000
+    * Das Ergebnis liegt zwischen 1 und 1000, inkl. Grenzen
+    */
+   public List<Integer> quadratRange1to1000(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+         //list.add ...
+      return list;
+   }
+
+   /** 5. Alle Kubikzahlen für die Zahlen 1 bis 10
+    *  Den Loop können Sie frei wählen
+    */
+   public List<Integer> kubik1to10(){
+      List<Integer> list = new ArrayList<>();
+      //hier loop ergänzen
+         //list.add ...
+      return list;
+   }
+}
